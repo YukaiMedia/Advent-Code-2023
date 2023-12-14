0ef1788cda9070dd0ba24a8c74d5b496ac313d77
pierrick herve <pierrick.herve@avanade.com>
2023-12-14 18:26:26 +0100

    test

--- jour 1/code phase 2.ps1
+++ jour 1/code phase 2.ps1
@@ -4,7 +4,7 @@ code phase 2.ps1
 
 $Code01		=	import-csv -Path ".\code01.csv" -Header code  # import list
 $NumCode	=	$code01 | select -expand code
-$NumCode	=	$numcode -replace "[0-9]", ""
+# $NumCode	=	$numcode -replace "[0-9]", ""
 $numcode    =   $numcode -replace "one", "1"
 $numcode    =   $numcode -replace "two", "2"
 $numcode    =   $numcode -replace "three", "3"
@@ -14,7 +14,7 @@ $numcode    =   $numcode -replace "six", "6"
 $numcode    =   $numcode -replace "seven", "7"
 $numcode    =   $numcode -replace "eight", "8"
 $numcode    =   $numcode -replace "nine", "9"
-$NumCode	=	$Code01.code -replace "[a-zA-Z]", ""	# remove alphas
+$NumCode	=	$numcode -replace "[a-zA-Z]", ""	# remove alphas
 
 
 foreach ($line in $NumCode)
