48e957b9dee779d87cf5c900e5b68eb43e6d1cba
Pierrick Herve <pierrick.herve@avanade.com>
2023-12-17 03:02:29 +0100

    test

---
+++ jour 2/Code phase 1.ps1
@@ -0,0 +1,7 @@
+# begin the code 01...
+
+. ./includes.ps1
+
+$inputCode		=	Get-Content .\input.txt
+$total			=	0
+
--- jour 2/includes.ps1
+++
@@ -1,6 +0,0 @@
-# begin the code 01...
-
-. ./includes.ps1
-
-$inputCode		=	Get-Content .\input.txt
-$total			=	0
